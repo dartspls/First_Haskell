a62badf52401e3f67ead78442070e27ce07d7d7f
Daniel Shepherd <darts100@gmail.com>
2021-07-15 14:02:45 +1200

    finished chaper 4

--- chap4.hs
+++ chap4.hs
@@ -17,3 +17,66 @@ sayMe x = "Not between 1 and 5"
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (pred n)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors a b = (fst a + fst b, snd a + snd b)
+
+addVectors' :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors' (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, x, _) = x
+
+third :: (a, b, c) -> c
+third (_, _, x) = x
+
+head' :: [p] -> p
+head' [] = error "can't call head on empty list"
+head' (x : _) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is emppty"
+tell [x] = "The list has one element: " ++ show x
+tell [x, y] = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x : y : _) = "The list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_ : xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x : xs) = x + sum' xs
+
+-- some kind of reduce?
+sum'' :: (Foldable t, Num b) => t b -> b
+sum'' = foldr (+) 0
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+  | a > b = GT
+  | a == b = EQ
+  | otherwise = LT
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+  where
+    (f : _) = firstname
+    (l : _) = lastname
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+  where
+    bmi weight height = weight / height ^ 2
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+   in sideArea + 2 * topArea
