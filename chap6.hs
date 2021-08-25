90e1bf98ba9d5f0d3de9d9c964173479a62d7dd5
= <darts100@gmail.com>
2021-08-25 22:31:53 +1200

    Half way through chap6

--- chap6.hs
+++ chap6.hs
@@ -1,8 +1,59 @@
-isUpperAlphanum :: Char -> Bool
-isUpperAlphanum = (`elem` ['A' .. 'Z'])
-
+-- Curried functions. Create new functions using existing functions with partial application
 multThree :: (Num a) => a -> a -> a -> a
 multThree x y z = x * y * z
 
+-- Partially apply multThree by supplying the first argument
+multTwoWithNine :: (Num a) => a -> a -> a
+multTwoWithNine = multThree 9
+
+multWithEighteen :: (Num a) => a -> a
+multWithEighteen = multTwoWithNine 2
+
+-- Partially apply the compare function. We can leave out the x variable because
+-- it would be on the right hand side on both sides of the equation
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = compare 100
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A' .. 'Z'])
+
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+-- a, b and c can all be the same type.
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
+
+-- swaps the order of the first two arguments to function f
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f x y = f y x
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x : xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x : xs)
+  | p x = x : filter p xs
+  | otherwise = filter p xs
+
+quicksort'' :: (Ord a) => [a] -> [a]
+quicksort'' [] = []
+quicksort'' (x : xs) = quicksort'' ys ++ [x] ++ quicksort'' zs
+  where
+    ys = filter' (<= x) xs
+    zs = filter' (> x) xs
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000, 99999 ..])
+  where
+    p x = x `mod` 3829 == 0
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' p (x : xs)
+  | p x = x : takeWhile' p xs
+  | otherwise = []
